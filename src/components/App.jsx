a3da5264bd45a96e6d30c295168f38884b54d70a
beligrous <beligrous@gmail.com>
2023-01-20 16:30:05 +0200

    add markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Form, Button, Container } from './App.styled';
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  getSubmitContact(e) {
+    e.preventDefault();
+    console.log(e.elements.value);
+  }
+
+  addContact() {}
+
+  render() {
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <Form>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <Button type="submit" onClick={() => this.getSubmitContact}>
+            Add contact
+          </Button>
+        </Form>
+        <div>
+          <h2>Contacts</h2>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </Container>
+    );
+  }
+}
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+  margin: 0 auto;
+  padding: 0;
+`;
+
+export const Button = styled.button`
+  width: 100px;
+  display: inline-block;
+  padding: 10px;
+  margin: 10px;
+`;
+
+export const Container = styled.div`
+  width: 600px;
+  text-align: center;
+  margin: 50px auto;
+  border: 2px solid black;
+  background-color: beige;
+`;
